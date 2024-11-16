1764c50932e29bf973d768fa983acfec567cae27
seiichiro-fujimaki <seiichiro.fujimaki@gmail.com>
2024-11-16 20:11:08 +0900

    ad2

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <p>heall shinada</p>
+        <p>hello shinada</p>
       </header>
     </div>
   );
